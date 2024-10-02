1bc3b1caaca80bd81e0f09696c5fffa0be8e0f24
Lucas-RM <lucas.marcondes36@gmail.com>
2024-10-02 19:29:47 -0300

    Função de Subtração

--- src/App.js
+++ src/App.js
@@ -31,16 +31,31 @@ const App = () => {
     }
   };
 
+  const handleMinusNumbers = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('-');
+    } else {
+      const minus = Number(firstNumber) - Number(currentNumber);
+      setCurrentNumber(String(minus));
+      setOperation('');
+    }
+  };
+
   const handleEquals = () => {
     if (firstNumber !== '0' && operation !== '' && currentNumber !== '0') {
       switch(operation){
         case '+':
           handleSumNumbers();
           break;
+        case '-':
+          handleMinusNumbers();
+          break;
         default: 
           break;
       }
-    } 
+    }
   };
 
   return (
@@ -50,14 +65,14 @@ const App = () => {
         <Row>
           <Button label="x" />
           <Button label="/" />
-          <Button label="C" onClick={handleOnClear} />
-          <Button label="X" />
+          <Button label="c" onClick={handleOnClear} />
+          <Button label="." />
         </Row>
         <Row>
           <Button label="7" onClick={() => handleAddNumber('7')} />
           <Button label="8" onClick={() => handleAddNumber('8')} />
           <Button label="9" onClick={() => handleAddNumber('9')} />
-          <Button label="-" />
+          <Button label="-" onClick={handleMinusNumbers} />
         </Row>
         <Row>
           <Button label="4" onClick={() => handleAddNumber('4')} />
